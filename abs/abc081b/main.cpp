2b20c9fc07d9651b0a261561548b439d5c063c6a
cactus163 <sy.numata@gmail.com>
2024-12-21 17:04:23 +0900

    新しいAtCoder問題の解法を追加し、VSCodeの設定ファイルを更新

---
+++ abs/abc081b/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n)
+{
+    int ans = 0;
+    while(n%2 == 0)
+    {
+        n /= 2;
+        ans++;
+    }
+}
+
+int main()
+{
+ int n;
+ cin >> n;
+ vector<int> a(n);
+ for (int i=0; i<n;i++) cin >> a[i];
+ int ans = 1000000000;
+ for(int i = 0; i < n; i++)
+ {
+     ans = min(ans, solve(a[i]));
+ }
+    cout << ans << endl;
+}
